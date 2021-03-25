36dd2a7ccdf7bfcbe36b0db6667842ce6bd8bef1
ADHSoft <adhsoft0@gmail.com>
2021-03-25 03:05:28 -0300

    Update typingAni.js

--- typingAni.js
+++ typingAni.js
@@ -1,7 +1,7 @@
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
-const typingAni = async (id=null,skip=0,skipf=0, speed =1) => {
+const typingAni = async (id=null,skip=0,skipf=0,speed=1) => {
 
     texto = new String;
     t2 = new String;
@@ -18,15 +18,18 @@ const typingAni = async (id=null,skip=0,skipf=0, speed =1) => {
         if (speed!=100) await delay((40+i*2)/speed);        
 
     }
-    await delay(150);
-    texto = t2;
-    t2="";
-    for (i=0 ; i<texto.length-skipf ; i++ )
-    {
-        t2+=texto.charAt(i);
-        
+    if (skipf!=0) {
+        await delay(150);
+        texto = t2;
+        t2="";
+        for (i=0 ; i<texto.length-skipf ; i++ )
+        {
+            t2+=texto.charAt(i);
+            
+        }
+        document.getElementById(id).textContent=t2;
     }
-    document.getElementById(id).textContent=t2;
+    delete t2, texto;
 
     
 };
